ef74194adad051a125b476b56dc88afca36e778b
HunterMcAfee <huntmac10@gmail.com>
2017-08-15 17:36:38 -0400

    Styled homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,17 +2,45 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const HomePageStyling = styled.div`
+    display: flex;
+    justify-content: center;
+    align-content: space-around;
+    flex-wrap: wrap;
+    padding-bottom: 40px;
+    h1 {
+        font-family: 'Press Start 2P', cursive;
+    }
+`;
+
+const HomePageContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`;
+
 class Homepage extends Component {
     render() {
         return (
-            <div>
-                    <h1 className='bitFont welcome'>Welcome</h1>
-                    <h1 className='bitFont welcome'>to</h1>
-                    <h1 className='bitFont welcome'>TwitchParty</h1>
+            <HomePageStyling>
+                <HomePageContainer>
+                    <h1>Welcome</h1>
+                </HomePageContainer>
+                <HomePageContainer>
+                    <h1>to</h1>
+                </HomePageContainer>
+                <HomePageContainer>
+                    <h1>TwitchParty</h1>
+                </HomePageContainer>
+                <HomePageContainer>
                     <Link to='/parties/'><button className='normalButton'>GO TO PARTIES</button></Link>
+                </HomePageContainer>    
+                <br /><br /><br />
+                <HomePageContainer>
                     <Link to='/users'><button className='normalButton'>USER LOGIN</button></Link>
-             
-            </div>
+                </HomePageContainer>
+            </HomePageStyling>
         );
     }
 }
